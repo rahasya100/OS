e8ceb739214b7080434da533a2b9ef463525d571
rahasya100 <neupane.rahasya14@gmail.com>
2019-03-02 20:19:19 +0530

    1st commit

---
+++ main1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+#include<iostream>
+using namespace std;
+main()
+{
+ int x,i,j,k,cyd,temp,max;             //initialixze
+ int head,array[x];                    //array list
+ int  total=0,dif;
+ printf("Enter maximum size of disk");
+              //take a input from user of maximum size of disk
+ scanf("%d",&max);
+
+ printf("\nEnter the  maximum size of an array: ");   //takes maximum size of an array
+ scanf("%d",&x);
+
+ a:
+   printf("\nEnter the current head position: ");       // current head position:
+   scanf("%d",&head);
+
+    if(head>max||head<=0)                               //conditional statement
+         {printf("Enter  it again");
+          goto a
+        }
